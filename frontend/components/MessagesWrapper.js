b937a5130c3a94662fffadf085da1bd11d28fd41
tomi.karikj <t.karikj@prtech.mk>
2024-05-14 08:10:26 +0200

    set the ID screen label

--- frontend/components/MessagesWrapper.js
+++ frontend/components/MessagesWrapper.js
@@ -23,6 +23,12 @@ class MessagesWrapper extends React.Component {
   }
 
   componentDidMount() {
+    const idScreen = document.getElementById('identificationScreen')
+    if (idScreen) {
+      idScreen.innerText = this.context.intl.formatMessage({
+        id: 'perun.plugin.svarog-notifications', defaultMessage: 'perun.plugin.svarog-notifications'
+      })
+    }
     this.getContextMenu(this.state.contextMenuItems);
     this.setState({ inboxComponent: <InboxComponent location={this.props.location} /> })
   }
